849a0766eaed3c6c2d414c2d5468e36206d909a2
julhoang <64108232+julhoang@users.noreply.github.com>
2024-09-19 09:42:13 -0700

    Web 1.0 hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import HeroSection from "@/app/sections/HeroSection";
 
 export default function Home() {
     return (
-        <main>
-            <HeroSection />
+        <main className="flex w-full text-center justify-center">
+            <div className="flex w-full max-w-7xl">
+                <HeroSection />
+            </div>
         </main>
     );
 }
--- src/app/sections/HeroSection.tsx
+++ src/app/sections/HeroSection.tsx
@@ -22,9 +22,26 @@ export default function HeroSection() {
                 </p>
             </div>
 
-            <p className="font-medium text-xl mt-10 text-gray-600">
+            {/* <p className="font-medium text-xl mt-10 text-gray-600">
                 Scroll down to check out my projects, some silly games, and more!
             </p>
+
+            <div className="animate-bounce animate-infinite animate-ease-in-out">
+                <svg
+                    className="w-8 h-15 text-grey-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="grey"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M19 9l-7 7-7-7"
+                    />
+                </svg>
+            </div> */}
         </section>
     );
 }
