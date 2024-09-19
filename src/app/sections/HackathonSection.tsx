7da98b47f2744827b826af7042c0d50277f587c3
julhoang <64108232+julhoang@users.noreply.github.com>
2024-09-19 16:28:17 -0700

    Web 1.0 - Hackathon section

--- src/app/sections/HackathonSection.tsx
+++ src/app/sections/HackathonSection.tsx
@@ -39,56 +39,76 @@ const hackathons: Hackathon[] = [
     },
 ];
 
-function ProjectCard({
-    media,
-    competition,
-    projectName,
-    description,
-    stack,
-    url,
-    demo,
-}: Hackathon) {
+function ProjectCard({ competition, projectName, description, stack, url, demo }: Hackathon) {
     return (
         <div className="flex flex-col gap-2">
-            <p className="font-semibold text-sm">{competition}</p>
-            <h4 className="font-bold text-3xl my-2">{projectName}</h4>
-            <p
-                className="whitespace-normal"
-                dangerouslySetInnerHTML={{ __html: description }}
-            ></p>
+            <div className="flex flex-col justify-between h-full">
+                <div>
+                    <p className="font-semibold text-sm">{competition}</p>
+                    <h4 className="font-bold text-3xl my-2 text-blue-800">{projectName}</h4>
+                    <p
+                        className="whitespace-normal"
+                        dangerouslySetInnerHTML={{ __html: description }}
+                    ></p>
 
-            <div>
-                {stack.map((tech) => (
-                    <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-                        {tech}
-                    </span>
-                ))}
+                    <div className="my-5">
+                        {stack.map((tech) => (
+                            <span
+                                key={tech}
+                                className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
+                            >
+                                {tech}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="flex flex-col md:flex-row md:gap-0">
+                    {demo && (
+                        <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                            View Demo
+                        </button>
+                    )}
+
+                    <button
+                        type="button"
+                        className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2 w-fit"
+                    >
+                        <svg
+                            className="w-4 h-4 me-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                        View Code on GitHub
+                    </button>
+                </div>
             </div>
-
-            {demo && (
-                <a
-                    href={demo}
-                    className="text-blue-500 hover:underline"
-                >
-                    View Demo
-                </a>
-            )}
-            <a href={url}>View Project</a>
         </div>
     );
 }
 
 export default function HackathonSection() {
     return (
-        <div className="bg-slate-100 p-10 text-left">
-            <h2 className="font-bold text-5xl md:text-7xl mb-2">hackathons.</h2>
+        <div className="bg-slate-50 p-10 text-left">
+            <h2 className="font-bold text-5xl md:text-7xl mb-2 text-blue-700">hackathons.</h2>
             <p className="text-lg md:text-xl md:w-1/2 mb-4 md:mb-8">
                 Adrenaline-fueled competitions, powered by too much coffee ☕, greasy pizza 🍕, and
                 a mountain of salty chips 🥨.
             </p>
 
-            <div className="grid md:grid-rows-3 divide-y-2">
-                <div className="md:grid md:grid-cols-2 py-10 md:gap-10">
+            <div className="grid md:grid-rows-3 divide-y">
+                <div className="md:grid md:grid-cols-2 py-10 md:gap-10 h-fit">
                     <video
                         width={"100%"}
                         src={hackathons[0].media}
@@ -98,7 +118,7 @@ export default function HackathonSection() {
                     <ProjectCard {...hackathons[0]} />
                 </div>
 
-                <div className="flex flex-col-reverse md:grid md:grid-cols-2 py-10 md:gap-10">
+                <div className="flex flex-col-reverse md:grid md:grid-cols-2 py-10 md:gap-10 h-fit">
                     <ProjectCard {...hackathons[1]} />
                     <video
                         width={"100%"}
@@ -108,7 +128,7 @@ export default function HackathonSection() {
                     />
                 </div>
 
-                <div className="md:grid md:grid-cols-2 py-10 md:gap-10">
+                <div className="md:grid md:grid-cols-2 py-10 md:gap-10 h-fit">
                     <video
                         width={"100%"}
                         src={hackathons[0].media}
--- src/app/sections/HeroSection.tsx
+++ src/app/sections/HeroSection.tsx
@@ -2,17 +2,20 @@ import Image from "next/image";
 
 export default function HeroSection() {
     return (
-        <section className="flex flex-col items-center justify-center min-h-screen p-8 md:p-20 w-full border">
+        <section className="flex flex-col items-center justify-center min-h-screen p-8 md:p-20 w-full border-4 border-slate-50">
             <p className="font-medium text-md md:text-xl text-gray-800">Hey there, I’m</p>
             <Image
                 src="/julia.png"
                 alt="Julia Hoang"
                 width={350}
                 height={350}
+                priority={true}
                 className="rounded-full"
             />
             <h1 className="font-bold text-6xl md:text-9xl pb-3 -mt-5 text-center">Julia Hoang</h1>
-            <p className="font-medium text-xl md:text-2xl">Software Developer based in Vancouver 🇨🇦</p>
+            <p className="font-medium text-xl md:text-2xl">
+                Software Developer based in Vancouver 🇨🇦
+            </p>
 
             <div className="w-full md:w-1/2 text-center mt-5">
                 <p className="text-md md:text-xl mt-4">
@@ -21,27 +24,6 @@ export default function HeroSection() {
                     websites, I’m likely rallying on the tennis court 🎾.
                 </p>
             </div>
-
-            {/* <p className="font-medium text-xl mt-10 text-gray-600">
-                Scroll down to check out my projects, some silly games, and more!
-            </p>
-
-            <div className="animate-bounce animate-infinite animate-ease-in-out">
-                <svg
-                    className="w-8 h-15 text-grey-600"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="grey"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M19 9l-7 7-7-7"
-                    />
-                </svg>
-            </div> */}
         </section>
     );
 }
